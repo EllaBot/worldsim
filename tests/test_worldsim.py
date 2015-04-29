a8b1589d619a8993fd6faf02b4666ee9a4d1a29b
Rishi Shah <rishihahs@gmail.com>
2015-04-29 16:33:04 -0500

    Add more tests for tick

--- tests/test_worldsim.py
+++ tests/test_worldsim.py
@@ -6,9 +6,23 @@ class TestWorldSim(object):
 
     def test_tick(self):
         world = WorldSim(10.0, 10.0, 5.0, 5.0)
-
         world.linear_velocity = 1.0
         world.angular_velocity = 0.0
         world.tick()
-
+        # Test just the linear change
         assert_equal(world.x, 5.0 + WorldSim.TICK_DURATION)
+
+        world = WorldSim(10.0, 10.0, 5.0, 5.0)
+        world.linear_velocity = 0.0
+        world.angular_velocity = 1.0
+        world.tick()
+        # Test just the angular change
+        assert_equal(world.theta, WorldSim.TICK_DURATION)
+
+        world = WorldSim(10.0, 10.0, 5.0, 5.0)
+        world.linear_velocity = 1.0
+        world.angular_velocity = 0.0
+        for x in range(12):
+            world.tick()
+        # Should not have overstepped boundary
+        assert_equal(world.x, 10.0)
