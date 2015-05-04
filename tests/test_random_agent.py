c10703a3c397807052a8f96de8ae16f62c937c95
Nick Walker <nick@nickwalker.us>
2015-05-04 17:54:20 -0500

    Rename agent tests

--- tests/test_random_agent.py
+++ tests/test_random_agent.py
@@ -7,7 +7,7 @@ import math
 from nose.tools import assert_equal
 from nose.tools import assert_almost_equal
 
-class TestWorldSim(object):
+class TestRandomAgent(object):
 
     def test_state(self):
         task = SearchTask(5, 5)
@@ -17,14 +17,12 @@ class TestWorldSim(object):
         assert_almost_equal(state.distance, 1)
         assert_almost_equal(state.omega, 0)
 
-        problem = SearchTask(5, 5)
         world = WorldSim(10, 10, 4, 5, task)
         agent = RandomAgent(world, task)
         state = agent.getstate()
         assert_almost_equal(state.distance, 1)
         assert_almost_equal(state.omega, math.pi)
 
-        problem = SearchTask(5, 5)
         world = WorldSim(10, 10, 5, 6, task)
         agent = RandomAgent(world, task)
 
