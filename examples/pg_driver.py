bb89042970d4012f7738a8b69b6d923baf5732f1
Nick Walker <nick@nickwalker.us>
2015-05-13 15:49:53 -0500

    Move position randomization into WorldSim
    Update driver with distance graphing

--- examples/pg_driver.py
+++ examples/pg_driver.py
@@ -7,26 +7,26 @@ from worldsim.tasks import SearchTask
 from worldsim.experiments.reward_plot import RewardPlot
 
 
-EPISODES = 600000
+EPISODES = 100000
 GOODRANDOMPOSITION = [0.575, -0.13, 0.0287, -0.897, -0.3881, 0.02335]
 
 
 def main():
     task = SearchTask(None, None, max_x=10.0, max_y=10.0)
-    world = VisualizedWorldSim(10.0, 10.0, 8.0, 3.0, task)
+    world = VisualizedWorldSim(10.0, 10.0, 8.0, 5.0, task, randomizeposition=True)
     agent = PGAgent(world, task, initialtheta=GOODRANDOMPOSITION, epsilon=0.4)
     world.agent = agent
     graph = RewardPlot()
 
     for episode in range(0, EPISODES):
-        #world.x = 8.0
-        #world.y = 3.0
         task = SearchTask(None, None, max_x=10.0, max_y=10.0)
+        #task = SearchTask(5.0, 5.0)
         world.task = task
         agent.task = task
-
+        d = task.distance(world.x, world.y)
         reward, steps = executeepisode(world, agent)
-        graph.plot(reward)
+        assert d/float(steps) <= world.TICK_DURATION * 1.5
+        graph.plot(d/float(steps))
 
     graph.freeze()
 
--- worldsim/worldsim.py
+++ worldsim/worldsim.py
@@ -29,7 +29,7 @@ class WorldSim(object):
     initial_y: float, optional
         The initial y position in meters, where the origin is the bottom left corner
     """
-    def __init__(self, width, height,  initial_x=5.0, initial_y=5.0, task=SearchTask(5.0, 5.0)):
+    def __init__(self, width, height,  initial_x=None, initial_y=None, task=SearchTask(5.0, 5.0), randomizeposition=False):
         self.width = width
         self.height = height
         self.task = task
@@ -37,6 +37,10 @@ class WorldSim(object):
         self.x = initial_x
         self.y = initial_y
         self.theta = 0.0
+
+        self._default_x = initial_x
+        self._default_y = initial_y
+        self._randomizeposition = randomizeposition
 
     def applyaction(self, action):
         """Tick the clock once based on TICK_DURATION
@@ -50,6 +54,10 @@ class WorldSim(object):
         self.y = min(max(self.y, 0.0), self.height)
 
     def reset(self):
-        self.x = random() * self.width
-        self.y = random() * self.height
+        if self._randomizeposition:
+            self.x = random() * self.width
+            self.y = random() * self.height
+        else:
+            self.x = self._default_x
+            self.y = self._default_y
         self.theta = 0.0
