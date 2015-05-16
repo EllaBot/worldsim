7a7be7337da042c296765f81d960f4fce06ce471
Rishi Shah <rishihahs@gmail.com>
2015-05-15 22:31:37 -0500

    ActorCritic Agent

--- worldsim/agents/__init__.py
+++ worldsim/agents/__init__.py
@@ -1,7 +1,8 @@
 from .random_agent import RandomAgent
 from .sarsa_agent import SarsaAgent
+from .actorcritic_agent import ActorCriticAgent
 from .pg_agent import PGAgent
 from .state import State
 from .action import Action
 
-__all__ = ['RandomAgent', 'SarsaAgent', 'PGAgent', 'State', 'Action']
+__all__ = ['RandomAgent', 'SarsaAgent', 'ActorCriticAgent', 'PGAgent', 'State', 'Action']
